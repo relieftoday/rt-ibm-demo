eaafa3099dfb4c5fdbf96d00f711306219f0448b
vRathore42 <vrathore42.dev@gmail.com>
2020-07-31 22:16:05 +0530

    Folder structure changed

---
+++ build/precache-manifest.b65c788855e4844319267c5350367f9f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d9c2e4f49804c5787ce45d3bd5329f1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "60605e8e75fa5527c22b",
+    "url": "/static/css/2.6eff3e32.chunk.css"
+  },
+  {
+    "revision": "ac73ebb28ddf307dbcac",
+    "url": "/static/css/main.eb702081.chunk.css"
+  },
+  {
+    "revision": "60605e8e75fa5527c22b",
+    "url": "/static/js/2.97ae99a5.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/static/js/2.97ae99a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac73ebb28ddf307dbcac",
+    "url": "/static/js/main.1b1c0315.chunk.js"
+  },
+  {
+    "revision": "496b596ccf97ceee91eb",
+    "url": "/static/js/runtime-main.1313eef0.js"
+  },
+  {
+    "revision": "bad6345fd73484b2c9673111215a7f78",
+    "url": "/static/media/marker.bad6345f.png"
+  }
+]);
