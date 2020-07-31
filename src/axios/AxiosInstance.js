b8ec9694386dad3703a1f750374ba95e3877c450
vRathore42 <vrathore42.dev@gmail.com>
2020-07-31 17:17:02 +0530

    changing server details

--- src/axios/AxiosInstance.js
+++ src/axios/AxiosInstance.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const URL = process.env.NODE_ENV === 'production'? "http://localhost/api" : "http://localhost:3001/api";
+const URL = process.env.NODE_ENV === 'production'? "http://demo.relief.today/api" : "http://localhost:3001/api";
 export default axios.create({
   baseURL: URL
 });
